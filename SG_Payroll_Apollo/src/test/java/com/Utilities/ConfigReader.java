2947b38c6013955ca80616141383145546e1857c
Sundar V <sundar@talentoz.com>
2021-11-03 18:41:57 +0530

    Updating on Base classes

--- SG_Payroll_Apollo/src/test/java/com/Utilities/ConfigReader.java
+++ SG_Payroll_Apollo/src/test/java/com/Utilities/ConfigReader.java
@@ -1,5 +1,20 @@
 package com.Utilities;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ConfigReader {
+	
+	Properties prop;
+
+	public void ConfigSetup(String Path) throws Exception {
+		FileInputStream FIS = new FileInputStream(Path);
+	    prop = new Properties();
+		prop.load(FIS);
+	}
+	
+	public String GetConfigValue(String Key) {
+		return prop.getProperty(Key);
+	}
 
 }
--- SG_Payroll_Apollo/src/test/java/com/Utilities/ExcelReader.java
+++ SG_Payroll_Apollo/src/test/java/com/Utilities/ExcelReader.java
@@ -1,5 +1,50 @@
 package com.Utilities;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelReader {
 
+	XSSFWorkbook WB;
+	
+	public void ExcelSetup(String Path) throws Exception {
+		FileInputStream FIS = new FileInputStream(Path);
+	    WB = new XSSFWorkbook(FIS);
+	}
+	
+	public String GetStringCellValue(String sheet, int row, int cell) {
+		String CellValue = WB.getSheet(sheet).getRow(row).getCell(cell).getStringCellValue();
+		return CellValue;
+	}
+	
+	public double GetNumericCellValue(String sheet, int row, int cell) {
+		double CellValue = WB.getSheet(sheet).getRow(row).getCell(cell).getNumericCellValue();
+		return CellValue;
+	}
+	
+	public int GetTotalRowNumebers(String sheet) {
+		int intialrows = WB.getSheet(sheet).getPhysicalNumberOfRows();
+		int actualrows = intialrows - 1;
+		return actualrows;
+	}
+	
+	public void WriteMyData(String path, String sheet, int row, int cell, String value) throws Exception {
+		FileInputStream FIS = new FileInputStream(path);
+		XSSFWorkbook WB2 = new XSSFWorkbook(FIS);
+		XSSFSheet Sheet = WB2.getSheet(sheet);
+		Sheet.createRow(row).createCell(cell).setCellValue(value);
+		FileOutputStream FIO = new FileOutputStream(path);
+		WB2.write(FIO);
+		WB2.close();
+	}
+	
+	public void CloseDoc() throws IOException {
+		WB.close();
+	}
+	
+	
 }
